fe875b508cf3d3f5780febb1ab4b3526609bca78
Hao1229 <s0939859391@gmail.com>
2020-07-23 13:45:01 +0800

    feat: add webpack module and plugin

---
+++ index.js
@@ -0,0 +1,2 @@
+import './src/js/all.js'
+import './src/scss/all.scss'
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,71 @@
-const path = require('path');
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
 module.exports = {
-  entry: './src/index.js',
-  // 進入點，所以檔案必須與此檔案有關聯才會被編譯
+  entry: './index.js',
   output: {
-    filename: 'index.bundle.js',
-    // 編譯檔案名稱
+    filename: 'js/[name].[hash].js',
     path: path.resolve(__dirname, 'dist')
-    // 編譯檔案的位置
-  }
-};
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin()
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../'
+            }
+          },
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: 'img/[name].[ext]',
+              limit: 8192
+            }
+          },
+          {
+            loader: 'image-webpack-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[hash].css'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new CleanWebpackPlugin()
+  ]
+}
